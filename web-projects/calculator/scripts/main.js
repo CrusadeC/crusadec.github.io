c8b9fd7d1809f2dfc4e9e23c98e34d79b5376604
Crusade ZK <crusadechong@outlook.com>
2024-01-04 02:52:45 +0800

    Calculator main.js
    - Removed button
    - Added basic description

--- web-projects/calculator/scripts/main.js
+++ web-projects/calculator/scripts/main.js
@@ -2,7 +2,6 @@ let myButton = document.querySelector("button");
 let userNumberInput0, userNumberInput1;
 let number0, number1;
 let setOperation;
-let operation = ["+", "-", "x", "*", "/"];
 let answer;
 
 userNumberInput0 = prompt("Please enter the first value.", 0);
@@ -11,6 +10,25 @@ userNumberInput1 = prompt("Please enter the second value.", 0);
 
 number0 = Number(userNumberInput0);
 number1 = Number(userNumberInput1);
-operation = Number(setOperation);
 
-output.log (number0, number1, String(operation));
+if (setOperation == "+") {
+    answer = number0 + number1;
+    alert(answer);
+}
+else if (setOperation == "-") {
+    answer = number0 - number1;
+    alert(answer);
+}
+else if (setOperation == "x" || "*") {
+    answer = number0 * number1;
+    alert(answer);
+}
+else if (setOperation == "/") {
+    answer = number0 / number1;
+    alert(answer);
+}
+else {
+    setOperation = prompt("Please enter an operator (+, -, x, * OR /).");
+ }
+
+console.log (number0, number1, setOperation, answer);
