8f6321d3b2b7d4bffbff337836b914bd46132cba
Crusade ZK <crusadechong@outlook.com>
2024-01-04 02:31:50 +0800

    Calculator main.js
    - Removed all function
    - Renamed variable
    - Testing user input without math

--- web-projects/calculator/scripts/main.js
+++ web-projects/calculator/scripts/main.js
@@ -1,71 +1,16 @@
 let myButton = document.querySelector("button");
-let userInput1;
-let userInput2;
-let num0;
-let num1;
+let userNumberInput0, userNumberInput1;
+let number0, number1;
 let setOperation;
 let operation = ["+", "-", "x", "*", "/"];
 let answer;
 
-function getNumber0() {
-    const num0 = prompt("Please enter the first value.");
-}
+userNumberInput0 = prompt("Please enter the first value.", 0);
+setOperation = prompt("Please enter an operator (+, -, x, * OR /).");
+userNumberInput1 = prompt("Please enter the second value.", 0);
 
-function getNumber1() {
-    const num1 = prompt("Please enter the second value.");
-}
+number0 = Number(userNumberInput0);
+number1 = Number(userNumberInput1);
+operation = Number(setOperation);
 
-function getOperator() {
-    const setOperation = prompt("Please enter an operator (+, -, x, * OR /).");
-}
-function getOperatorAgain() {
-    const setOperation = prompt("The operator you have entered are unavailable. Please enter an operator again (+, -, x, * OR /).");
-}
-
-function add(num0, num1) {
-    let answer = num0 + num1;
-    return answer;
-}
-function subtract(num0, num1) {
-    let answer = num0 - num1;
-    return answer;
-}
-function multiply(num0, num1) {
-    let answer = num0 * num1;
-    return answer;
-}
-function divide(num0, num1) {
-    let answer = num0 / num1;
-    return answer;
-}
-myButton.onclick = function () {
-    getNumber0();
-
-    if (!localStorage.getItem("number0")) {
-        getNumber0();
-    } else {
-        getOperator();
-    }
-
-    if (!localStorage.getItem("setOperation")) {
-        getOperator();
-    } else {
-        getNumber1();
-    }
-
-    if (!localStorage.getItem("number1")) {
-        getNumber1();
-    }
-
-    if (setOperation === operation[0]) {
-        answer = num0 + num1;
-    } else if (setOperation === operation[1]) {
-        answer = num0 - num1;
-    } else if (setOperation === operation[2] || operation[3]) {
-        answer = num0 * num1;
-    } else if (setOperation === operation[4]) {
-        answer = num0 / num1;
-    }
-
-    alert(answer);
-}
+output.log (number0, number1, String(operation));
